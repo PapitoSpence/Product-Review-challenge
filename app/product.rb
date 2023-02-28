d99b6a993f7acfb53a4c0ab17a6758af7e87c2e1
PapitoSpence <PapitoSpence>
2023-02-28 13:29:17 +0300

    completed

--- app/product.rb
+++ app/product.rb
@@ -1,4 +1,4 @@
-#product class that inherits from activerecord
+# product class that inherits from activerecord
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
